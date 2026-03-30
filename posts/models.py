ee86ec11ba9c2cf711665ea4bae275a23b5e9a18
zzhou370 <zzhou370@gmail.com>
2026-03-30 14:49:58 +0800

    initial commit

---
+++ posts/models.py
@@ -0,0 +1,6 @@
+# posts/models.py
+from django.db import models
+class Post(models.Model):
+    text = models.TextField()
+    def __str__(self): # new
+        return self.text[:50]
---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+# posts/urls.py
+from django.urls import path
+# from .views import post_list
+from .views import PostList # new
+urlpatterns = [
+ # path("", post_list, name="post_list"),
+    path("", PostList.as_view(), name="home"), # new
+]
